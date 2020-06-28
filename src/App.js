5253fee1963571d7ea3df3f8e1593f84df90d35d
tianyu5177 <zhangtianyu5177@sina.com>
2020-06-29 07:58:10 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-	/* 
-			备注：调用setState会让react去更新状态，状态更新完毕之后react调用render方法去更新页面
+/* 
+	备注：调用setState会让react去更新状态，状态更新完毕之后react调用render方法去更新页面
 
-			深入理解setState:
-					明确：setState()更新状态的两种写法:
-
-						(1).setState(stateChange,[callback])
-										1.stateChange是状态改变对象(可以体现出状态的更改)
-										2.callback是可选的回调函数，它在状态更新完毕、界面也更新完毕后，被调用。
+	深入理解setState:
+			明确：setState()更新状态的两种写法:
+				(1).setState(stateChange,[callback])
+							1.stateChange是状态改变对象(可以体现出状态的更改)
+							2.callback是可选的回调函数，它在状态更新完毕、界面也更新完毕后，被调用。
 											
 	*/
 
