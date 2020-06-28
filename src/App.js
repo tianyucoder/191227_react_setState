b19b09557af78f82274fc29b1bc9534b0aa1aa86
tianyu5177 <zhangtianyu5177@sina.com>
2020-06-28 17:00:01 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	/* 
+			备注：调用setState会让react去更新状态，状态更新完毕之后react调用render方法去更新页面
+
+			深入理解setState:
+					明确：setState()更新状态的两种写法:
+
+						(1).setState(stateChange,[callback])
+										1.stateChange是状态改变对象(可以体现出状态的更改)
+										2.callback是可选的回调函数，它在状态更新完毕、界面也更新完毕后，被调用。
+											
+	*/
+
+	state = {
+		count:0
+	}
+
+	add = ()=>{
+		const {count} = this.state
+		this.setState({count:count+1},()=>{console.log('setState所指定的回调执行了');})
+	}
+
+	render() {
+		console.log('render执行了');
+		return (
+			<div>
+				<h1>当前求和为:{this.state.count}</h1>
+				<button onClick={this.add}>点我加1</button>
+			</div>
+		)
+	}
 }
-
-export default App;
